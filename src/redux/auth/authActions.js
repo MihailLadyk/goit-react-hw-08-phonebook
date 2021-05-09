1fad958e92d79a7e8cd1bda988ed9b2887047553
MihailLadyk <lp346426201@gmail.com>
2021-05-09 10:25:09 +0300

    hw_8

--- src/redux/auth/authActions.js
+++ src/redux/auth/authActions.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const loginRequest = createAction("auth/loginRequest");
 export const loginSuccess = createAction("auth/loginSuccess");
 export const loginError = createAction("auth/loginError");
